4b43dc9a692ba89917e400a52b27eec79a6e767b
Sarish Paul <Sarish.Paul@directed.com.au>
2021-04-21 09:28:06 +1000

    Fix Ids

--- PersonService/Models/Event.cs
+++ PersonService/Models/Event.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace PersonService.Models
@@ -11,6 +12,7 @@ namespace PersonService.Models
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
+        [JsonIgnore]
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime TimeStamp { get; set; }
